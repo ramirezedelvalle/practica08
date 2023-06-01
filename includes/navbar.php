d52bc2dba5dbc60c5603439338f7357ee5fddb21
Nozen <ramirezedelvalle@gmail.com>
2023-05-31 19:58:55 -0600

    inicializacion de los archivos finales

---
+++ includes/navbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">Tienda virtual</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Clientes
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/practica_8/vistas/clientes/index.php">Agregar Cliente</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/practica_8/vistas/clientes/buscar.php">Buscar Cliente</a>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ vistas/clientes/index.php
@@ -0,0 +1,28 @@
+
+<?php include_once '../../includes/header.php'?>
+<?php include_once '../../includes/navbar.php'?>
+    <div class="container">
+        <h1 class="text-center">Formulario de clientes</h1>
+        <div class="row justify-content-center">
+            <form action="/practica_8/controladores/clientes/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="cliente_nombre">Nombre del Cliente</label>
+                        <input type="text" name="cliente_nombre" id="cliente_nombre" class="form-control">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="cliente_nit">Nit del Cliente</label>
+                        <input type="text" step="0.01" min="0" name="cliente_nit" id="cliente_nit" class="form-control">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <button type="submit" class="btn btn-primary w-100">Guardar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+<?php include_once '../../includes/footer.php'?>
